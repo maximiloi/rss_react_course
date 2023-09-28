9ea44dab2ef198bc8fdd3a03677e311cfdb7ffad
iloi <iloi@iMac-iloi.local>
2023-09-28 14:17:53 +0300

    feat: add Eslint + airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
 function App() {
-
   return (
-    <>
+    <p>
       Hello, RSS
-    </>
-  )
+    </p>
+  );
 }
 
-export default App
+export default App;
