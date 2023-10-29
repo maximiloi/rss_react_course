4536939a8ce8e4eea2a2bdd4274ddd6b8158a0c4
maximiloi <maximiloi@gmail.com>
2023-10-29 16:23:13 +0300

    feat: add type and check/update state

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,16 +2,60 @@ import { Component } from 'react';
 
 import './style.scss';
 
-class Search extends Component {
+interface InputValueProps {}
+
+interface InputValueState {
+  inputValue: string;
+}
+
+class Search extends Component<InputValueProps, InputValueState> {
+  constructor(props: InputValueProps) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleSearch = () => {
+    const { inputValue } = this.state;
+
+    if (inputValue) {
+      localStorage.setItem('React-Components', inputValue);
+      const inputHtmlElem = document.querySelector(
+        '.search__input'
+      ) as HTMLInputElement;
+      if (inputHtmlElem) inputHtmlElem.value = '';
+    }
+  };
+
+  handleKeyPress = (event: { key: string }) => {
+    if (event.key === 'Enter') {
+      this.handleSearch();
+    }
+  };
+
+  handleInputChange = (event: { target: { value: string } }) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
   render() {
+    const { inputValue } = this.state;
+
     return (
       <div className="search" data-testid="search-component">
         <input
           type="text"
           className="search__input"
           placeholder="search movie..."
+          value={inputValue}
+          onChange={this.handleInputChange}
+          onKeyPress={this.handleKeyPress}
         />
-        <button type="button" className="search__button">
+        <button
+          type="button"
+          className="search__button"
+          onClick={this.handleSearch}
+        >
           search
         </button>
       </div>
