e7948f0aee0011e86ff06ce6a5ea95d763d78beb
maximiloi <maximiloi@gmail.com>
2023-10-29 16:19:15 +0300

    feat: add type and componentDidMount

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,37 @@
 import { Component } from 'react';
+import LocalStorage from '../../helper/localStorage';
 
 import './style.scss';
 
-class Main extends Component {
+interface Props {}
+
+interface State {
+  data: [] | string | null;
+}
+
+class Main extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount(): void {
+    const value = LocalStorage.getResult();
+    if (value === undefined) {
+      this.setState({ data: 'type in title of movie in English' });
+    } else {
+      this.setState({ data: value });
+    }
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <main>
-        <h2>my main</h2>
+        <h2>{data}</h2>
       </main>
     );
   }
