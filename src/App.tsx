eb26926804566d8de679e0685446b7cf89edbb10
maximiloi <maximiloi@gmail.com>
2023-10-29 16:04:26 +0300

    refactor: component App to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+      </>
+    );
+  }
 }
 
 export default App;
