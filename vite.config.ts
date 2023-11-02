a0244309c97097e9ea89554b7213627f69cda667
maximiloi <maximiloi@gmail.com>
2023-11-02 10:15:43 +0300

    config: add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,18 @@
 
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@helper': path.resolve(__dirname, './src/helper'),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
