900af8cdfc6ce24606d17c037b3bb292764a649a
maximiloi <maximiloi@gmail.com>
2023-11-03 20:40:58 +0300

    fix: number type

--- src/helper/apiResponce.ts
+++ src/helper/apiResponce.ts
@@ -1,5 +1,5 @@
 class ApiResponse {
-  static async fetchData(value: string, pageNumber = 1) {
+  static async fetchData(value: string, pageNumber: number) {
     const url = `https://www.omdbapi.com/?apikey=67e1bb9b&s=${value}&page=${pageNumber}`;
     const response = await fetch(url);
     const data = await response.json();
