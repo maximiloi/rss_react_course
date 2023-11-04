bafc0aaf435721379505d51fc01d9007d7fb4b0c
maximiloi <maximiloi@gmail.com>
2023-11-04 17:10:09 +0300

    feat: change type to func feach

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useEffect, useState } from 'react';
 import Spin from '@components/Spin';
+import Item from '@components/Item';
 import Pagination from '@components/Pagination';
 import ApiResponse from '@helper/apiResponce';
 import LocalStorage from '@helper/localStorage';
@@ -21,12 +24,13 @@ function Card({ searchWord }: Props) {
   const [loading, setLoading] = useState<boolean>(true);
   const [itemArray, setItemArray] = useState<ICard[] | null>(null);
   const [totalCards, setTotalCards] = useState<string>('');
+  const [imdbIDCard, setImdbIDCard] = useState<string>('');
 
-  const fetchData = async (keyword: string, pageNumber = 1) => {
+  const fetchCardsData = async (keyword: string, pageNumber = '1') => {
     const response: {
       Search: ICard[];
       totalResults: string;
-    } = await ApiResponse.fetchData(keyword, pageNumber);
+    } = await ApiResponse.fetchCardsData(keyword, pageNumber);
 
     setItemArray(response.Search);
     setTotalCards(response.totalResults);
@@ -36,43 +40,54 @@ function Card({ searchWord }: Props) {
   const handlePageChange = (pageNumber: number) => {
     const valueLocalStorage = LocalStorage.getResult();
     if (typeof valueLocalStorage === 'string') {
-      fetchData(valueLocalStorage, pageNumber);
+      fetchCardsData(valueLocalStorage, pageNumber.toString());
     }
+  };
+
+  const handleCardItemClick = (itemId: string) => {
+    setImdbIDCard(itemId);
   };
 
   useEffect(() => {
     const valueLocalStorage = LocalStorage.getResult();
     if (typeof valueLocalStorage === 'string') {
-      fetchData(valueLocalStorage);
+      fetchCardsData(valueLocalStorage);
     }
   }, []);
 
   useEffect(() => {
     if (searchWord) {
-      fetchData(searchWord);
+      fetchCardsData(searchWord);
     }
   }, [searchWord]);
 
   return (
     <>
-      <div className="card card__wrapper">
-        {loading ? (
-          <Spin />
-        ) : (
-          itemArray?.map((item: ICard) => (
-            <div className="card__item" key={item.imdbID}>
-              <img
-                className="card__img"
-                src={item.Poster}
-                alt={item.Title}
-                width="182px"
-                height="268px"
-              />
-              <p>{item.Year}</p>
-              <h3>{item.Title}</h3>
-            </div>
-          ))
-        )}
+      <div className="cards cards__wrapper">
+        <div className="card card__list">
+          {loading ? (
+            <Spin />
+          ) : (
+            itemArray?.map((item: ICard) => (
+              <div
+                className="card__item"
+                key={item.imdbID}
+                onClick={() => handleCardItemClick(item.imdbID)}
+              >
+                <img
+                  className="card__img"
+                  src={item.Poster}
+                  alt={item.Title}
+                  width="182px"
+                  height="268px"
+                />
+                <p>{item.Year}</p>
+                <h3>{item.Title}</h3>
+              </div>
+            ))
+          )}
+        </div>
+        <Item IdIMDB={imdbIDCard} />
       </div>
       <Pagination
         totalCards={totalCards}
