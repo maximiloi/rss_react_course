7c99312d27fc81b8c577fc9dfa3f7fdcbfc87bc5
maximiloi <maximiloi@gmail.com>
2023-11-04 16:19:53 +0300

    refactor: class function

--- src/helper/apiResponce.ts
+++ src/helper/apiResponce.ts
@@ -1,10 +1,21 @@
+const API_KEY = '67e1bb9b';
+const API_URL = 'https://www.omdbapi.com/';
+
 class ApiResponse {
-  static async fetchData(value: string, pageNumber: number) {
-    const url = `https://www.omdbapi.com/?apikey=67e1bb9b&s=${value}&page=${pageNumber}`;
+  static async fetchData(url: string) {
     const response = await fetch(url);
     const data = await response.json();
-
     return data;
+  }
+
+  static async fetchCardsData(value: string, pageNumber: string) {
+    const url = `${API_URL}?apikey=${API_KEY}&s=${value}&page=${pageNumber}`;
+    return ApiResponse.fetchData(url);
+  }
+
+  static async fetchItemData(IdIMDB: string) {
+    const url = `${API_URL}?apikey=${API_KEY}&i=${IdIMDB}`;
+    return ApiResponse.fetchData(url);
   }
 }
 
