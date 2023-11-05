c0496203fa001fb324e7a1bd7a9f839597b7352c
maximiloi <maximiloi@gmail.com>
2023-11-05 23:54:39 +0300

    refactor: getResult func

--- src/helper/localStorage.ts
+++ src/helper/localStorage.ts
@@ -1,9 +1,6 @@
 class LocalStorage {
-  static getResult(): string | void | null {
-    if (localStorage.getItem('name-cinema-iloi')) {
-      return localStorage.getItem('name-cinema-iloi');
-    }
-    return localStorage.setItem('name-cinema-iloi', '');
+  static getResult() {
+    return localStorage.getItem('name-cinema-iloi');
   }
 
   static setResult(inputValue: string) {
