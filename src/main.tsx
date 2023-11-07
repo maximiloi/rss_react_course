662419037054706484d38400a4a76fb188d1900f
maximiloi <maximiloi@gmail.com>
2023-11-07 17:50:21 +0300

    feat: delete StrictMode, add children createRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from '@components/Error';
+import Main from '@components/Main';
+
 import App from './App';
 
 import './index.scss';
@@ -11,11 +13,12 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [{ index: true, element: <Main /> }],
   },
 ]);
 
 ReactDOM.createRoot(document.querySelector('#app')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
 );
