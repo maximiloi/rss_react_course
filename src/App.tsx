15b6016900d8ed3d242eedbed33397946fc0fb5a
maximiloi <maximiloi@gmail.com>
2023-11-07 17:52:43 +0300

    refactor: delete transfer ptops, and add Outlet router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { SetStateAction, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '@components/Header';
-import Main from '@components/Main';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearchChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setSearchTerm(event.target.value);
-  };
-
   return (
     <>
-      <Header onSearchChange={handleSearchChange} />
-      <Main searchTitle={searchTerm} />
+      <Header />
+      <Outlet />
     </>
   );
 }
