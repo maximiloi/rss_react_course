66f7bce7cc074f89dde8c9369eb347168af883e0
maximiloi <maximiloi@gmail.com>
2023-11-10 09:10:43 +0300

    feat: types moved external file

---
+++ src/components/Item/type.ts
@@ -0,0 +1,13 @@
+export interface ItemInterface {
+  Poster: string;
+  Title: string;
+  Released: string;
+  Genre: string;
+  Director: string;
+  Actors: string;
+  Plot: string;
+  Type: string;
+  imdbRating: number;
+  Response: string;
+  Error: string;
+}
