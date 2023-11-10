1434afdeb4ba9ddc622b644683d159775c4410fe
maximiloi <maximiloi@gmail.com>
2023-11-10 10:46:37 +0300

    fix: remove rule

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useEffect, useState } from 'react';
 import { useSearchParams, useLocation, Outlet, Link } from 'react-router-dom';
 
