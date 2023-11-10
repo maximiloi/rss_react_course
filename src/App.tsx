1fbb6d4f498b9ff3e2a2e5b40d0b4bb4c2d46533
maximiloi <maximiloi@gmail.com>
2023-11-10 14:42:43 +0300

    feat: add AppContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useMemo, useState } from 'react';
+import AppContext, { AppContextType } from '@context/AppContext';
 import Header from '@components/Header';
 import Main from '@components/Main';
 
 function App() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const appContextValue: AppContextType = useMemo(
+    () => ({ searchValue, setSearchValue, loading, setLoading }),
+    [loading, searchValue]
+  );
+
   return (
-    <>
+    <AppContext.Provider value={appContextValue}>
       <Header />
       <Main />
-    </>
+    </AppContext.Provider>
   );
 }
 
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,13 @@
 import { createContext } from 'react';
 
-const AppContext = createContext({
+export type AppContextType = {
+  searchValue: string;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const AppContext = createContext<AppContextType>({
   searchValue: '',
   setSearchValue: () => {},
   loading: true,
