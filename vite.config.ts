4c58663849e46775170e4e3346123366c9cc7163
maximiloi <maximiloi@gmail.com>
2023-11-10 12:56:44 +0300

    config: add new alias context

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
       '@helper': path.resolve(__dirname, './src/helper'),
     },
   },
