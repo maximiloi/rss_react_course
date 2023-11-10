587171a6b670be3bbf9152b9a59799216e164fd5
maximiloi <maximiloi@gmail.com>
2023-11-10 10:35:11 +0300

    refactor: component add loader rrd

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,80 +1,29 @@
-import { useEffect, useState } from 'react';
-import Spin from '@components/Spin';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import ApiResponse from '@helper/apiResponce';
+import setRatingColor from '@helper/setRatingColor';
 
+import { ItemInterface, ParamsInterface } from './type';
 import './style.scss';
 
-interface ItemInterface {
-  Poster: string;
-  Title: string;
-  Released: string;
-  Genre: string;
-  Director: string;
-  Actors: string;
-  Plot: string;
-  Type: string;
-  imdbRating: number;
-}
+export async function loaderCardData({ params }: { params: ParamsInterface }) {
+  const itemResponce: ItemInterface = await ApiResponse.fetchItemData(
+    params.itemId
+  );
 
-interface Props {
-  IdIMDB: string;
-  searchWord: string;
-  pageChange: number;
-}
-
-function Item({ IdIMDB, searchWord, pageChange }: Props) {
-  const [totalCards, setTotalCards] = useState<ItemInterface | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isItemVisible, setItemIsVisible] = useState(true);
-
-  const fetchCardData = async () => {
-    try {
-      setIsLoading(true);
-      const response = await ApiResponse.fetchItemData(IdIMDB);
-      setTotalCards(response);
-    } catch (error) {
-      console.error('Error fetching item data:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const getRatingColor = (rating: number) => {
-    const ratingNumber =
-      typeof rating === 'number' ? rating : parseFloat(rating);
-    if (ratingNumber) {
-      if (ratingNumber >= 7.6) {
-        return <span className="green">{rating}</span>;
-      }
-      if (ratingNumber >= 5.5 && ratingNumber < 7.5) {
-        return <span className="yellow">{rating}</span>;
-      }
-    }
-    return <span className="red">{rating}</span>;
-  };
-
-  const handleUnmount = () => {
-    setItemIsVisible(false);
-  };
-
-  useEffect(() => {
-    if (IdIMDB) {
-      fetchCardData();
-      setItemIsVisible(true);
-    }
-  }, [IdIMDB]);
-
-  useEffect(() => {
-    setItemIsVisible(false);
-  }, [searchWord, pageChange]);
-
-  if (isLoading) {
-    return <Spin />;
+  if (itemResponce.Response === 'True') {
+    return { itemResponce };
   }
 
-  if (!totalCards) {
-    return '';
-  }
+  throw new Error(`${itemResponce.Error}`);
+}
+
+function Item() {
+  const { itemResponce } = useLoaderData() as { itemResponce: ItemInterface };
+  const navigate = useNavigate();
+
+  const closeItem = () => {
+    navigate(-1);
+  };
 
   const {
     Poster,
@@ -86,30 +35,28 @@ function Item({ IdIMDB, searchWord, pageChange }: Props) {
     Plot,
     imdbRating,
     Type,
-  } = totalCards;
+  } = itemResponce;
 
   return (
     <div className="item">
-      {isItemVisible && (
-        <div className="item__box">
-          <button className="item__close" type="button" onClick={handleUnmount}>
-            Close
-          </button>
-          <img src={Poster} alt={Title} className="item__img-top" />
-          <div className="item__body">
-            <span className="item__type">Type: {Type}</span>
-            <h3 className="item__title">{Title}</h3>
-            <p className="item__text">
-              IMDb Rating: {getRatingColor(imdbRating)}
-            </p>
-            <p className="item__text">Premiere: {Released}</p>
-            <p className="item__text">Genre: {Genre}</p>
-            <p className="item__text">Director: {Director}</p>
-            <p className="item__text">Actors: {Actors}</p>
-            <p className="item__text">Plot: {Plot}</p>
-          </div>
+      <div className="item__box">
+        <button className="item__close" type="button" onClick={closeItem}>
+          Close
+        </button>
+        <img src={Poster} alt={Title} className="item__img-top" />
+        <div className="item__body">
+          <span className="item__type">Type: {Type}</span>
+          <h3 className="item__title">{Title}</h3>
+          <p className="item__text">
+            IMDb Rating: {setRatingColor(imdbRating)}
+          </p>
+          <p className="item__text">Premiere: {Released}</p>
+          <p className="item__text">Genre: {Genre}</p>
+          <p className="item__text">Director: {Director}</p>
+          <p className="item__text">Actors: {Actors}</p>
+          <p className="item__text">Plot: {Plot}</p>
         </div>
-      )}
+      </div>
     </div>
   );
 }
