a8925f256b7f3c665aa633467a592a75031ac8ba
maximiloi <maximiloi@gmail.com>
2023-11-11 21:51:19 +0300

    refactor: title test

--- src/components/Header/index.test.jsx
+++ src/components/Header/index.test.jsx
@@ -3,12 +3,17 @@ import { render, screen } from '@testing-library/react';
 
 import Header from './index';
 
-describe('Header', () => {
-  it('renders "Hello, RSS" title and search component', () => {
+describe('Header component', () => {
+  it('displays title', () => {
     render(<Header />);
 
     const titleElement = screen.getByRole('heading', { level: 1 });
+    expect(titleElement).toBeInTheDocument();
     expect(titleElement).toHaveTextContent('Hello, RSS React student');
+  });
+
+  it('renders search component', () => {
+    render(<Header />);
 
     const searchElement = screen.getByTestId('search-component');
     expect(searchElement).toBeInTheDocument();
