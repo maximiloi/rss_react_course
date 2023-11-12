a1013770147885f8101e242259b2826334415539
maximiloi <maximiloi@gmail.com>
2023-11-12 16:38:08 +0300

    fix: wrapped BrowserRouter

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,19 +1,28 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
+import { BrowserRouter } from 'react-router-dom';
 import Header from './index';
 
 describe('Header component', () => {
   it('displays title', () => {
-    render(<Header />);
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
 
     const titleElement = screen.getByRole('heading', { level: 1 });
     expect(titleElement).toBeInTheDocument();
     expect(titleElement).toHaveTextContent('Hello, RSS React student');
   });
 
-  it('renders search component', () => {
-    render(<Header />);
+  it('renders Search component', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
 
     const searchElement = screen.getByTestId('search-component');
     expect(searchElement).toBeInTheDocument();
