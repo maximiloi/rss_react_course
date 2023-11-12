07907074e830271fd1b0830abc4bdb56cb45ecb6
maximiloi <maximiloi@gmail.com>
2023-11-12 16:36:53 +0300

    fix: wrapped BrowserRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App', () => {
   it('renders Header and Main components', () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
 
     const headerElement = screen.getByRole('banner');
     expect(headerElement).toBeInTheDocument();
