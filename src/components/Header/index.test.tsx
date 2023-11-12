9100ace9f7e459cf353a09f410d70fcc69ed8692
maximiloi <maximiloi@gmail.com>
2023-11-12 21:04:12 +0300

    feat: add husky pre-push

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
 import { BrowserRouter } from 'react-router-dom';
+
 import Header from './index';
 
 describe('Header component', () => {
--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
 import { BrowserRouter } from 'react-router-dom';
+
 import AppContext, { AppContextType } from '../../context/AppContext';
 import Main from './index';
 
